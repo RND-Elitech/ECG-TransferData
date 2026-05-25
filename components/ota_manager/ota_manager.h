54870074ee0089e7940a1464c24708eb367c4c76
rivaldisinkoprima <rivaldi.sinkoprima@gmail.com>
2026-05-25 16:32:58 +0700

    up version

--- components/ota_manager/ota_manager.h
+++ components/ota_manager/ota_manager.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 /* ─── Versi firmware saat ini ─── */
-#define APP_VERSION "1.0.6"
+#define APP_VERSION "1.0.7"
 
 /* ─── Supabase Project Credentials ─── */
 /* Ganti dengan URL dan Anon Key dari Supabase Project Settings -> API Anda */
